649ef18a63da0afad3af0928e13b723e08385506
José filho ribeiro <87453124+Jfilhoribeiro@users.noreply.github.com>
2021-08-31 13:26:07 -0300

    add files the project

---
+++ netflix-clone-dbz/scripts/owl/setup.js
@@ -0,0 +1,37 @@
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if(event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expaned', active);
+    if(active) {
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
+}
+
+btnMobile.addEventListener('click' , toggleMenu);
+btnMobile.addEventListener('touchstart' , toggleMenu);
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
